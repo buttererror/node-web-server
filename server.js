77fa63b0d9d2f5315dbac5d1656cb7e80045e14f
Mahmoud <sswsy2006@gmail.com>
2018-05-29 18:42:34 +0200

    handlebarsjs and express.js

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + "/public"));
+
+let currentYear = new Date().getFullYear();
 
 app.get('/', (req, res) => {
    // res.send("<h1>hello Express</h1>");
-   res.send({
-      name: "Mahmoud",
-      likes: [
-          "Ping Pong",
-          "adventure"
-      ]
+   // res.send({
+   //    name: "Mahmoud",
+   //    likes: [
+   //        "Ping Pong",
+   //        "adventure"
+   //    ]
+   // });
+   res.render('home.hbs', {
+      pageTitle: "Home page",
+      currentYear,
+      welcomeMessage: "Hello viewers"
    });
 });
 
 app.get('/about', (req, res) => { // handler callback when make a request to this path
-   res.send("About page");
+   // res.send("About page");
+   res.render('about.hbs', {
+      pageTitle: "About page",
+      currentYear
+   });
 });
 
 app.get('/bad', (req, res) => {
